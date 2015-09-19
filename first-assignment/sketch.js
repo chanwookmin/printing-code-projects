b21432aa1f991c43f910c1519f4a4d6ce15aac61
Chanwook Min <chanwook.min@gmail.com>
2015-09-18 22:41:02 -0400

    first assignment

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -1,20 +1,21 @@
 var r = new Rune({
   container: "#canvas",
-  width: 800,
-  height: 800,
+  width: 612,
+  height: 792,
   debug: true
 });
 
-r.rect(100, 100, 200, 200)
-  .fill(255, 0, 0)
+r.rect(150, 160, 320,320)
+  .fill(0)
+  .stroke(0)
+  r.ellipse(260, 230, 260, 300)
+  .fill(255)
+  .stroke(255)
+
+r.triangle(170, 350, 300, 663 ,404, 200)
+  .fill(255)
   .stroke(0)
 
-r.triangle(400, 50, 400, 400, 650, 50)
-  .fill(0, 255, 0)
-  .stroke(0)
 
-r.ellipse(200, 500, 200, 300)
-  .fill(0, 0, 255)
-  .stroke(false)
 
 r.draw();
